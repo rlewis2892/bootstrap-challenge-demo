37e86636d67e5aad37a45c2a44e381239239712e
Rochelle Lewis <rlewis37@cnm.edu>
2017-05-16 09:27:43 -0600

    added Bootstrap navbar

--- index.php
+++ index.php
@@ -34,7 +34,45 @@
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
-			this is the body text
+
+			<header>
+				<nav class="navbar navbar-default">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Bootstrap Challenge Demo</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="main-menu">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="https://github.com/rlewis2892/bootstrap-challenge-demo" target="_blank">GitHub</a></li>
+								<li><a href="https://bootcamp-coders.cnm.edu/~rlewis37" target="_blank">RL Deployment</a></li>
+								<li><a href="https://angular.io/" target="_blank">Angular.io</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+
+			<section>
+
+			</section>
+
+			<section>
+
+			</section>
+
+			<section>
+
+			</section>
+
 		</div>
 		<footer>
 			this is the footer
